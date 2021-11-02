1ce34136bc7c3a1f480d71b89cf3fe242bea8c48
gabrielfndz <gabriel.omercador@hotmail.com>
2021-11-02 13:14:03 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
-    );
+    <>
+      <NavBar />
+      <div className="container">
+        <DataTable/>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return(
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">
+          App desenvolvido por <a href="https://github.com/gabrielfndz" target="_blank" rel="noreferrer">Gabriel Oliveira</a>
+        </p>
+        <p className="text-light">
+          <small><strong>Semana Spring React</strong></small>
+        </p>
+      </div>
+    </footer>
+  );
+}
