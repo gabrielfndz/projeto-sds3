b8d9a2090af7b6455a0be8642c7dfb5d31080d2a
gabrielfndz <gabriel.omercador@hotmail.com>
2021-11-07 23:27:52 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import Chart from "react-apexcharts";
 import { SaleSum } from "types/sale";
 import { BASE_URL } from "utils/requests";
@@ -10,23 +11,22 @@ type ChartData = {
 
 export default function DonutChart() {
 
-  // forma errada
-  let chartData : ChartData = { labels: [], series: []};
+  const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-  axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then(response => {
-      const data = response.data as SaleSum[];
-      const myLabels = data.map(x => x.sellerName);
-      const mySeries = data.map(x => x.sum);
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+      .then(response => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
+        setChartData({ labels: myLabels, series: mySeries });
+      });
+  }, [])
 
-      chartData = { labels: myLabels, series: mySeries };
-      console.log(chartData);
-    });
-  
-  const mockData = {
-      series: [477138, 499928, 444567, 220426, 473088],
-      labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé'],
-  };
+  // const mockData = {
+  //     series: [477138, 499928, 444567, 220426, 473088],
+  //     labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé'],
+  // };
 
   const options = {
     legend: {
@@ -35,9 +35,9 @@ export default function DonutChart() {
   }
 
   return (
-    <Chart 
-      options={{ ...options, labels: mockData.labels}}
-      series={mockData.series}
+    <Chart
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
       type="donut"
       height="240"
     />
